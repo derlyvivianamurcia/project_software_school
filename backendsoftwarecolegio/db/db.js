43876d5ecde04e8ce53038e417b9fdc63a11e3b0
jucamo0713 <jucamo.0713@gmail.com>
2021-02-02 21:31:31 -0500

    gitignore y cambios varios

--- backendsoftwarecolegio/db/db.js
+++ backendsoftwarecolegio/db/db.js
@@ -1,20 +1,20 @@
 const { Pool } = require('pg')
-
+require('dotenv').config();
 const pool = new Pool({
-  host: '35.225.234.240',
-  user: 'postgres',
-  password : '1234567890', 
-  database : 'geekacademy', 
+  host: process.env.HOST_DB,
+  user: process.env.USER_DB,
+  password: process.env.PASSWORD_DB,
+  database: process.env.DATABASE,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
 })
 
 pool.on('remove', client => {
-    console.log("===============")
-    //console.log(client)
-    //console.log("===============")
+  console.log("===============")
+  //console.log(client)
+  //console.log("===============")
 })
+console.log("Succesfull conection with db "+pool.options.database);
 
-
-module.exports = {pool}
+module.exports = { pool };
