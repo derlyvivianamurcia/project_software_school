9caedf6d0143b7fd3d4f0cc2f5a2207683b6ac10
Sebastian Jimenez <sebasg1325@hotmail.com>
2021-02-04 02:57:21 -0500

    routes estudiantes

---
+++ backendsoftwarecolegio/routes/routesEs.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const router = Router();
+
+const studentsController = require('../controllers/controllerStudents');
+
+router.get('/estudiantes', studentsController.listStudents);
+router.get('/estudiantes/:id', studentsController.getStudent);
+router.post('/estudiantes', studentsController.addStudent);
+router.put('/estudiantes/:id', studentsController.updateStudent);
+router.delete('/estudiantes/:id', studentsController.deleteStudent);
+
+module.exports = router;
